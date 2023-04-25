a18ab6a373c70696ebbbc4d3d7ec2c52478dca21
Suyash8844 <suyashgharge@gmail.com>
2023-04-25 08:08:37 +0530

    Integer Cache

---
+++ javaprogram/Array/Program20.java
@@ -0,0 +1,12 @@
+// Integer Cache(Auto Boxing)
+class Program20{
+	public static void main(String[] args){
+		int x=10;
+		int y=10;
+		Integer z=10;
+		System.out.println(System.identityHashCode(x));
+		System.out.println(System.identityHashCode(y));
+		System.out.println(System.identityHashCode(z));
+	}
+}
+
