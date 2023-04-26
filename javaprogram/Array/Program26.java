7504b82c33b1b7e4a6c7715eda75081ddb817456
Suyash8844 <suyashgharge@gmail.com>
2023-04-26 08:35:19 +0530

    Command Line Argument

---
+++ javaprogram/Array/Program26.java
@@ -0,0 +1,9 @@
+class Program26{
+	public static void main(String[] args){
+		int arr[]={10,20,30};
+		String friends[]={"yash","nachi","pashya","shirke"};
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
+}
