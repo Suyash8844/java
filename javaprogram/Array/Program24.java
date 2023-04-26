b566e5966b27b3a1c14e1cf3a5591b8f69b67706
Suyash8844 <suyashgharge@gmail.com>
2023-04-26 08:19:48 +0530

    reference

---
+++ javaprogram/Array/Program24.java
@@ -0,0 +1,18 @@
+class Program24{
+	static void fun(int xarr[]){
+		for(int x:xarr){
+			System.out.println(x);
+		}
+		xarr[0]=50;
+	}
+	public static void main(String[] args){
+		int arr[]={10,20,30};
+		for(int x:arr){
+			System.out.println(x);
+		}
+		fun(arr);
+		for(int x:arr){
+			System.out.println(x);
+		}
+	}
+}
