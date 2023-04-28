730347fc7f309f3dc13867c1f3efabfa453b787f
Suyash8844 <suyashgharge@gmail.com>
2023-04-28 09:23:06 +0530

    2D Array Length find

---
+++ javaprogram/Array/Program29.java
@@ -0,0 +1,8 @@
+class Program29{
+	public static void main(String[] args){
+		int arr[][]=new int[2][2];
+		int arr2[]=new int[2];
+		System.out.println(arr.length);
+		System.out.println(arr2.length);
+	}
+}
