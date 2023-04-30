e076392f94cfe2d7b20539f2eb574206df5e4f5b
Suyash8844 <suyashgharge@gmail.com>
2023-04-30 12:50:51 +0530

    Second Min

---
+++ javapractice/Array/Program20.java
@@ -0,0 +1,28 @@
+//second min element in the array
+import java.util.*;
+class Program20{
+        public static void main(String[] args){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Enter size of array");
+                int size=sc.nextInt();
+                System.out.println("Enter Elements of array");
+                int arr[]=new int[size];
+                for(int i=0;i<arr.length;i++){
+                        arr[i]=sc.nextInt();
+                }
+                if(size>2){
+                for(int i=0;i<arr.length;i++){
+                        for(int j=i+1;j<arr.length;j++){
+                                if(arr[i]>arr[j]){
+                                        int temp=arr[i];
+                                        arr[i]=arr[j];
+                                        arr[j]=temp;
+                                }
+                        }
+                }
+                }else{
+                        System.out.println("You have Entered size of "+size);
+                }
+                System.out.println("Second Minimum no:"+arr[1]);
+        }
+}
