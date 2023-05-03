d9094ea96c18ef2dd1e78359828e6b4600f633cc
Suyash8844 <suyashgharge@gmail.com>
2023-05-03 08:26:40 +0530

    String

---
+++ javaprogram/String/Program6.java
@@ -0,0 +1,11 @@
+class Program6{
+	public static void main(String[] args){
+		String str1="Suyash";
+		String str2="Gharge";
+		System.out.println(str1);
+		System.out.println(str2);
+		str1.concat(str2);
+		System.out.println(str1);
+                System.out.println(str2);
+	}
+}
