45743ab5e738019c3e5c9a25d3fb7b67ed8e84f2
Suyash8844 <suyashgharge@gmail.com>
2023-05-03 08:09:05 +0530

    String

---
+++ javaprogram/String/Program3.java
@@ -0,0 +1,16 @@
+class Program3{
+	public static void main(String[] args){
+		String str1="kanha";
+		String str2="kanha";
+		String str3=new String("kanha");
+		String str4=new String("kanha");
+		String str5=new String("Rahul");
+		String str6="Rahul";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+		System.out.println(str5);
+		System.out.println(str6);
+	}
+}
