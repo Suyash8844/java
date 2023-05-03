1ca14783c059610aa04c05b04f49e04bc9b14c4f
Suyash8844 <suyashgharge@gmail.com>
2023-05-03 08:16:46 +0530

    String

---
+++ javaprogram/String/Program4.java
@@ -0,0 +1,10 @@
+class Program4{
+	public static void main(String[] args){
+		String str1="kanha";
+		String str2=str1;
+		String str3=new String(str2);
+		System.out.println(System.identityHashCode(str1));
+		System.out.println(System.identityHashCode(str2));
+		System.out.println(System.identityHashCode(str3));
+	}
+}
