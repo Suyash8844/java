eeac516eeadc214f465320bc01e24f34b2de2059
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 10:50:36 +0530

    compareTo Method

---
+++ javaprogram/String/Method4.java
@@ -0,0 +1,15 @@
+/*
+method4:public int compareTo(String str2);
+parameter:String(second string)
+returntype:integer
+*/
+
+class Method4{
+	public static void main(String[] args){
+		String str1="Suyash";
+		String str2="Suyash";
+		String str3="suyash";
+		System.out.println(str1.compareTo(str2));
+		System.out.println(str2.compareTo(str3));
+	}
+}
