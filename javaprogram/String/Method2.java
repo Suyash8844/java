d8a3fda5e51f70b947e2019b6fa34dd96c10b792
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 10:34:55 +0530

    length Method

---
+++ javaprogram/String/Method2.java
@@ -0,0 +1,13 @@
+/*
+method2:public int length();
+parameter:No parameter
+return type:Integer
+*/
+
+
+class Method2{
+	public static void main(String[] args){
+		String str1="Core2Web";
+		System.out.println(str1.length());
+	}
+}
