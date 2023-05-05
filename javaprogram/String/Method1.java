ebfd03bd57d7a131977eaf46043bda8757cddeeb
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 10:29:57 +0530

    concat Method

---
+++ javaprogram/String/Method1.java
@@ -0,0 +1,13 @@
+/*method 1:public String concat(String str);
+parameter:String
+return type: String
+*/
+
+class Method1{
+	public static void main(String[] args){
+		String str1="Core2";
+		String str2="Web";
+		String str3=str1.concat(str2);
+		System.out.println(str3);
+	}
+}
