3d74173656f9030d9dfbf462904511b746be86b3
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 11:25:55 +0530

    indexOf Method

---
+++ javaprogram/String/Method8.java
@@ -0,0 +1,14 @@
+/*
+method8:public int indexOf(int ch,int fromIndex)
+parameter:character(ch to find),Integer(index to start the search)
+returntype:Integer
+*/
+
+class Method8{
+	public static void main(String[] args){
+		String str1="Shashi";
+		System.out.println(str1.indexOf('h',0));
+		System.out.println(str1.indexOf('h',1));
+		System.out.println(str1.indexOf('h',2));
+	}
+}
