d276e03487482c95fad6c59b9e79226d3a481d56
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 10:40:40 +0530

    charAt Method

---
+++ javaprogram/String/Method3.java
@@ -0,0 +1,13 @@
+
+/*method3:public charAt(int index);
+parameter:integer(index);
+return type:character
+*/
+
+class Method3{
+	public static void main(String[] args){
+		String str="Core2Web";
+		System.out.println(str.charAt(4));
+		System.out.println(str.charAt(0));
+	}
+}
