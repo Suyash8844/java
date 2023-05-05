3f097ef55dfc98b75c2def81afa353822ad01011
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 11:50:11 +0530

    substring Method

---
+++ javaprogram/String/Method11.java
@@ -0,0 +1,12 @@
+/*
+method11:public String substring(int index);
+parameter:Integer(index of the string)
+returntype:String
+*/
+class Method11{
+	public static void main(String[] args){
+		String str="Core2Web Tech";
+		System.out.println(str.substring(5));
+		System.out.println(str.substring(0,3));
+	}
+}
