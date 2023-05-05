91f3d08bad56564bd6cd47ea5077cf8cb77367b7
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 11:07:44 +0530

    equals Method

---
+++ javaprogram/String/Method6.java
@@ -0,0 +1,13 @@
+/*
+method6:public boolean equals(0bject anObject)
+paramter:Object(anObject)
+returntype:boolean
+*/
+
+class Method6{
+	public static void main(String[] args){
+		String str1="Shashi";
+		String str2=new String("Shashi");
+		System.out.println(str1.equals(str2));
+	}
+}
