97833e9594bfe01dbdad8a037edb8c9ab6b1c0af
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 11:16:02 +0530

    equalsIgnoreCase Method

---
+++ javaprogram/String/Method7.java
@@ -0,0 +1,14 @@
+/*
+method 7:public boolean equalsIgnoreCase(String another String);
+parameter: String(str2)
+returntype:boolean
+*/
+
+class Method7{
+	public static void main(String[] arge){
+		String str1="Suyash";
+		String str2="SUYASH";
+		System.out.println(str1.equalsIgnoreCase(str2));
+	}
+}
+
