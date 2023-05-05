dd086084c12ffd1fc642d8b48832e5d42fd477d7
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 11:44:02 +0530

    replace Method

---
+++ javaprogram/String/Method10.java
@@ -0,0 +1,13 @@
+/*
+method10:public String replace(char oldChar,char newChar);
+parameter:character(old Character),character(new Character)
+returntype:String
+
+*/
+
+class Method10{
+        public static void main(String[] args){
+                String str="Suyas";
+                System.out.println(str.replace('s','a'));
+        }
+}
