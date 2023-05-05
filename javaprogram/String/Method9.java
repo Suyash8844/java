01e2db8b1531ef12d22ed492b5806b5f5eb5d21c
Suyash8844 <suyashgharge@gmail.com>
2023-05-05 11:34:50 +0530

    lastIndexOf Method

---
+++ javaprogram/String/Method9.java
@@ -0,0 +1,12 @@
+/*
+method9:public int lastIndexOf(int ch,int uptoIndex);
+parameter:character(ch to find),Integer (index to starts the search)
+returntype:Integer
+*/
+
+class Method9{
+	public static void main(String[] args){
+		String str1="Shashi";
+		System.out.println(str1.lastIndexOf('h',0));
+	}
+}
