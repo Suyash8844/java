965a8a20d94daf6a8d2f28748634cd405c386802
Suyash8844 <suyashgharge@gmail.com>
2023-05-10 07:47:29 +0530

    StringBuffer

---
+++ javaprogram/String/StringBuffer/Program7.java
@@ -0,0 +1,12 @@
+class Program7{
+        public static void main(String[] args){
+                String str1="Suyash";
+                String str2=new String("Gharge");
+                StringBuffer str3=new StringBuffer("Core2Web");
+		str1.concat(str2);
+		str3.append(str2);
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
