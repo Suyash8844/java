a21423cf95de1bf17502792097afa605d5311c03
Suyash8844 <suyashgharge@gmail.com>
2023-05-10 07:49:07 +0530

    insert Method

---
+++ javaprogram/String/StringBuffer/Method2.java
@@ -0,0 +1,11 @@
+/* 
+method 2:public synchronized StringBuffer delete(int start,int end);
+*/
+class Method2{
+	public static void main(String[] args){
+		StringBuffer sb=new StringBuffer("ShashiCore2Web");
+		System.out.println(sb);
+		sb.insert(6,"Bagal");
+		System.out.println(sb);
+	}
+}
