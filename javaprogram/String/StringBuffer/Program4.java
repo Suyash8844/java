2f452dcf4108de3d973b80ed885228b95623ee65
Suyash8844 <suyashgharge@gmail.com>
2023-05-10 07:42:53 +0530

    StringBuffer

---
+++ javaprogram/String/StringBuffer/Program4.java
@@ -0,0 +1,14 @@
+class Program4{
+	public static void main(String[] args){
+
+		StringBuffer sb=new StringBuffer(100);
+
+		sb.append("Biencaps");
+		sb.append("Core2Web");
+		System.out.println(sb);
+		System.out.println(sb.capacity());
+		sb.append("Incubater");
+		System.out.println(sb);
+		System.out.println(sb.capacity());
+	}
+}
