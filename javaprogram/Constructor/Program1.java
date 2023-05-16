6abe217d887ceaa4dc85a1ae9e7048a80e503da8
Suyash8844 <suyashgharge@gmail.com>
2023-05-16 10:33:26 +0530

    Constructor eg

---
+++ javaprogram/Constructor/Program1.java
@@ -0,0 +1,14 @@
+class Core2Web{
+	Core2Web(){
+		System.out.println("In Core2Web");
+	      }
+        int x=10;	
+	public static void main(String args[]){
+		System.out.println("In Main");
+		Core2Web obj=new Core2Web();
+		System.out.println(obj.x);
+		System.out.println(obj.y);
+	}
+	int y=20;
+}
+
