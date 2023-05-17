25a7f9a0901a25f274de45d45b789cb579245b67
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 12:32:27 +0530

    startsWith

---
+++ javaprogram/String/Method12.java
@@ -0,0 +1,9 @@
+/*
+method12:public boolean startsWith(String prefix,int tooffset);
+*/
+class Method12{
+	public static void main(String[] args){
+		String str="Core2Web";
+		System.out.println(str.startsWith("or",1));
+	}
+}
