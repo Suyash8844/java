4f2d10d887b641bd29c38c25b8df7bca262e34fe
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 09:33:16 +0530

    class & object Example

---
+++ javaprogram/classesobject/Program3.java
@@ -0,0 +1,12 @@
+class Demo{
+	int x=10;
+	String str1="Suyash";
+	void fun(){
+		String str2="Suyash";
+		String str3=new String("Veridia");
+	}
+	public static void main(String[] args){
+		Demo obj=new Demo();
+		obj.fun();
+	}
+}
