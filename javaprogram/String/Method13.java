2884320537bed10c23611607a7ace534218052a4
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 12:34:05 +0530

    endsWith

---
+++ javaprogram/String/Method13.java
@@ -0,0 +1,10 @@
+/*
+method13:public boolean endsWith(String suffix);
+*/
+
+class Method13{
+	public static void main(String[] args){
+		String str1="Know the code till the core";
+		System.out.println(str1.endsWith("core"));
+	}
+}
