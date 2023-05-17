123dea6ecdb682206a72d9d0d7e3bd1da4089f9a
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 17:01:04 +0530

    replace

---
+++ javaprogram/String/StringBuffer/Method9.java
@@ -0,0 +1,8 @@
+class Method9{
+	public static void main(String[] args){
+		StringBuffer str1=new StringBuffer("Know the code untill the core");
+		System.out.println(str1);
+		str1.replace(14,20,"till");
+		System.out.println(str1);
+	}
+}
