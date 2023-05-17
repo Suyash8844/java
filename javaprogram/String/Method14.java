33b92369164a0af9eed331a416304d9e6297b385
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 12:34:38 +0530

    subString

---
+++ javaprogram/String/Method14.java
@@ -0,0 +1,10 @@
+/*
+method14:public String substring(int i, int j);
+*/
+
+class Method14{
+	public static void main(String[] args){
+		String str="Core2Web";
+		System.out.println(str.substring(5,8));
+	}
+}
