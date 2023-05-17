2e39baa929033a328d6ea799188fec8e2ec956f3
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 09:23:48 +0530

    IPL Example

---
+++ javaprogram/Program1.java
@@ -0,0 +1,11 @@
+class IPL{
+	int player=11;
+	void Cricket(){
+		System.out.println("CSK");
+	}
+	public static void main(String[] args){
+		IPL obj=new IPL();
+		obj.Cricket();
+		System.out.println(obj.player);
+	}
+}
