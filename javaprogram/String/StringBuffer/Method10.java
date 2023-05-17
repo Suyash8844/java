167c327cbbb598c1a687dcb639456cdc42162366
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 17:04:09 +0530

    substring

---
+++ javaprogram/String/StringBuffer/Method10.java
@@ -0,0 +1,7 @@
+class Method10{
+	public static void main(String[] args){
+		StringBuffer str1=new StringBuffer("Know the code till the core");
+		String str2=str1.substring(14,27);
+		System.out.println(str2);
+	}
+}
