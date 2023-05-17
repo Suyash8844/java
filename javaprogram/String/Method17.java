6040470f559073e329f7e2a484b4517b47d92053
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 12:36:22 +0530

    trim

---
+++ javaprogram/String/Method17.java
@@ -0,0 +1,9 @@
+/*
+method15:public string trim()
+*/
+class Method17{
+        public static void main(String[] args){
+                String str="Know the code till the core";
+                System.out.println(str.trim());
+        }
+}
