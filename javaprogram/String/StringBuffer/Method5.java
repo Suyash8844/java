2cb7abff72f1818a6f9208fff699613a366af4fb
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 16:48:38 +0530

    toString

---
+++ javaprogram/String/StringBuffer/Method5.java
@@ -0,0 +1,9 @@
+class Method5{
+	public static void main(String[] args){
+		StringBuffer str1=new StringBuffer("Know the code till the core");
+		String str2="Core2Web";
+		String str3=str1.toString();
+		String str4=str2.concat(str3);
+		System.out.println(str4);
+	}
+}
