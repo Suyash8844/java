f53e337fd2ee98967c7e5aa2b5f645d02d38f786
Suyash8844 <suyashgharge@gmail.com>
2023-05-17 12:35:49 +0530

    toUpperCase

---
+++ javaprogram/String/Method16.java
@@ -0,0 +1,10 @@
+/*
+method16:public string toUpperCase()
+*/
+
+class Method16{
+        public static void main(String[] args){
+                String str="Core2Web";
+                System.out.println(str.toUpperCase());
+        }
+}
